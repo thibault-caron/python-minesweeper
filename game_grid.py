c8a0ac6b97d78b535c0f03aad435b7d2be843ab0
Thibault Caron <thibault.caron@laplateforme.io>
2025-03-24 16:23:24 +0100

    Feat: init dev

---
+++ game_grid.py
@@ -0,0 +1,6 @@
+
+class GameGrid:
+    def __init__(self, rows, columns):
+        self.rows = rows
+        self.columns = columns
+
---
+++ main.py
@@ -0,0 +1,8 @@
+
+def main():
+    # game = Game()
+    # game.mainloop()
+    pass
+
+if __name__ == "__main__":
+    main()

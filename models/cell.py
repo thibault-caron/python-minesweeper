a21f2e9c9211384a0a4f81e17f8939301befe873
Thibault Caron <thibault.caron@laplateforme.io>
2025-03-25 11:28:41 +0100

    Feat: rename tile to cell

---
+++ models/cell.py
@@ -0,0 +1,12 @@
+class Cell:
+    def __init__(self, row: int, col: int):
+        self.row: int = row
+        self.col: int = col
+        self.is_mine: bool = False
+        self.is_revealed: bool = False
+        self.is_flagged: bool = False
+        self.is_unsure: bool = False
+        self.adjacent_mines: int = 0
+
+    def __repr__(self) -> str:
+        return f"Cell({self.row}, {self.col}, Mine={self.is_mine}, Flagged={self.is_flagged}, Unsure={self.is_unsure}, Adjacent Mines={self.adjacent_mines})"
